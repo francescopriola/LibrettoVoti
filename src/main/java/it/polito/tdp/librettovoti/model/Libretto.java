cfe34b1fd8c31ab1beb8ba7527ca58d297cab41f
francescopriola <francescopriola@localhost>
2022-03-07 15:20:24 +0100

    Primi 5 punti dell'esercizio

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class Libretto {
 
-	private List<Voto> voti ;
+	private List<Voto> voti;
 	
 	public Libretto() {
-		this.voti = new ArrayList<Voto>() ;
+		this.voti = new ArrayList<Voto>();
 	}
 	
 	public void add(Voto v) {
@@ -16,52 +16,45 @@ public class Libretto {
 	}
 	
 	public Libretto filtraPunti(int punti) {
-		Libretto result = new Libretto() ;
-		for(Voto v: this.voti) {
-			if(v.getPunti()==punti) {
+		Libretto result = new Libretto();
+		for(Voto v : this.voti) {
+			if(v.getPunti() == punti)
 				result.add(v);
-			}
 		}
-		return result ;
+		return result;
 	}
 	
 	/**
-	 * Restituisce il punteggio ottenuto all'esame di cui 
-	 * specifico il nome
+	 * Restituisce il punteggio ottenuto all'esame di cui specifico il nome
 	 * @param nome Nome dell'esame
 	 * @return punteggio numerico, oppure {@code null} se l'esame non esiste
 	 */
 	public Integer puntiEsame(String nome) {
-		for(Voto v: this.voti) {
-			if( v.getNome().equals(nome) ) {
-				return v.getPunti() ;
-			}
+		for(Voto v : this.voti) {
+			if(v.getNome().equals(nome))
+				return v.getPunti();
 		}
-//		return -1;
-		return null ;
-//		throw new IllegalArgumentException("Corso non trovato") ;
+		//return -1;
+		return null;
+		//throw new IllegalArgumentException("Corso non trovato!");
 	}
-	
 	
 	public boolean isDuplicato(Voto v) {
-		for(Voto v1: this.voti) {
+		for(Voto v1 : this.voti) {
 			if(v1.equals(v))
-				return true ;
+				return true;
 		}
-		return false ;
+		return false;
 	}
-	
 	
 	public boolean isConflitto(Voto v) {
-		Integer punti = this.puntiEsame(v.getNome()) ;
-		if (punti != null && punti != v.getPunti())
+		Integer punti = this.puntiEsame(v.getNome());
+		if(punti != null && punti != v.getPunti())
 			return true;
-		else
-			return false;
+		return false;
 	}
 	
-	
 	public String toString() {
-		return this.voti.toString() ;
+		return this.voti.toString();
 	}
 }
--- src/main/java/it/polito/tdp/librettovoti/model/Voto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Voto.java
@@ -3,39 +3,42 @@ package it.polito.tdp.librettovoti.model;
 import java.util.Objects;
 
 public class Voto {
-	private String nome ;
-	private int punti ;
+
+	private String nome;
+	private int punti;
 	
 	public Voto(String nome, int punti) {
 		super();
 		this.nome = nome;
 		this.punti = punti;
 	}
+	
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public int getPunti() {
 		return punti;
 	}
+	
 	public void setPunti(int punti) {
 		this.punti = punti;
 	}
-	
+
 	@Override
 	public String toString() {
-		return nome+" : "+ punti;
+		return nome + ": " + punti;
 	}
+
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
-		result = prime * result + punti;
-		return result;
+		return Objects.hash(nome, punti);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -45,17 +48,8 @@ public class Voto {
 		if (getClass() != obj.getClass())
 			return false;
 		Voto other = (Voto) obj;
-		if (nome == null) {
-			if (other.nome != null)
-				return false;
-		} else if (!nome.equals(other.nome))
-			return false;
-		if (punti != other.punti)
-			return false;
-		return true;
+		return Objects.equals(nome, other.nome) && punti == other.punti;
 	}
 	
 	
-	
-		
 }
